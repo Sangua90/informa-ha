88399161aaa349a837b5e1dc67cf94d4e7f294c4
Sangua90 <93098330+Sangua90@users.noreply.github.com>
2026-09-04 16:56:51 +0200

    InFormha 0.9.41 show all Health Auto Export metrics

---
+++ informa/health_dashboard_0941.py
@@ -0,0 +1,7 @@
+import health_dashboard_0940 as base
+import app as root
+
+app = base.app
+root.VERSION = "0.9.41"
+
+print("[INFORMHA_HEALTH_DASHBOARD_FIX] version=0.9.41 show_all_hae_metrics=1 dynamic_uncategorized=1", flush=True)
